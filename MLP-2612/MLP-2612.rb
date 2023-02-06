85069c46cba72c10ac33416e1020e15ca410a302
idakroniczaja <so.kida@hotmail.com>
2023-02-06 18:01:53 -0500

    change

--- MLP-2612/MLP-2612.rb
+++ MLP-2612/MLP-2612.rb
@@ -9,7 +9,7 @@ def remove_active_lab_appointment
 end
 
 def remove_active_lab_appointment
-    lab_appt = LaboratoryAppointment.find(cust_appointment_id: params[:cust_appointment_id])
+    lab_appt = LaboratoryAppointment.find_by(cust_appointment_id: params[:cust_appointment_id])
     if lab_appt.exists? 
         lab_appt.destroy 
         message = "Lab appointment is successfully deleted"
