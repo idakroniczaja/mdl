bdbe6128b1989e7708f355f149220939e0b86ce8
idakroniczaja <so.kida@hotmail.com>
2023-02-13 19:40:52 -0500

    change

--- MLP-2612/MLP-2612.rb
+++ MLP-2612/MLP-2612.rb
@@ -4,9 +4,9 @@ def remove_active_lab_appointment
     lab_appt = LaboratoryAppointment.find_by(cust_appointment_id: params[:cust_appointment_id])
     if lab_appt.exists? 
         lab_appt.destroy 
-        message = "Lab appointment is successfully deleted"
+        message = "Lab appointment is successfully deleted."
     else
-        message = "No lab appt related to this appointment"
+        message = "No lab appointment related to this appointment."
     end
     render json: {message: message} 
 end
